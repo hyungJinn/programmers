7ad7c4cc93d9aeb2eaf4aaf441ebeed03d47fa05
함형진 <73492591+hyungJinn@users.noreply.github.com>
2021-09-18 02:33:36 +0900

    Create solution.cpp

---
+++ Level2/문자열 압축/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = s.size();
+    
+    for(int i = 1; i <= s.size()/2; i++) {
+        string convert, temp;
+        
+        int cnt = 1; 
+        temp = s.substr(0, i);
+        for(int j = i; j < s.size(); j+=i) {
+            if(temp == s.substr(j, i)) cnt++;
+            else {
+                if(cnt > 1) convert += to_string(cnt);
+                convert += temp;
+                temp = s.substr(j, i);
+                cnt = 1;
+            }
+        }
+        if(cnt > 1) convert += to_string(cnt);
+        
+        convert+=temp;
+        //cout << i << endl;
+        //cout << convert << endl;
+        answer = (answer < convert.size() ? answer : convert.size());
+    }
+    return answer;
+}
